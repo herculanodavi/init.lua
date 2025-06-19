a5f1ba85b40564375eb5f31f42e6a5a6c04686d3
Davi Herculano <davi.herculano@adam-audio.de>
2025-06-19 09:58:14 +0200

    feature: add basic plugins and settings

---
+++ lua/apre/lazy/gitsigns.lua
@@ -0,0 +1,7 @@
+{
+    "lewis6991/gitsigns",
+    config = function()
+        local gitsigns = require('gitsigns')
+        -- still thinking about bindings here      
+    end
+}
--- lua/apre/set.lua
+++ lua/apre/set.lua
@@ -1,2 +1,19 @@
 vim.opt.guicursor = ""
 vim.opt.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.colorcolumn = "80"
