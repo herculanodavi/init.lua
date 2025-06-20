a6f585bc96d31c0aad28b1bb7bc766ea4f541495
Davi Herculano <herculanodavi@gmail.com>
2025-06-20 18:44:12 +0200

    fix: add return keyword and fix url

--- lua/apre/lazy/gitsigns.lua
+++ lua/apre/lazy/gitsigns.lua
@@ -1,5 +1,5 @@
-{
-    "lewis6991/gitsigns",
+return {
+    "lewis6991/gitsigns.nvim",
     config = function()
         local gitsigns = require('gitsigns')
         -- still thinking about bindings here      
